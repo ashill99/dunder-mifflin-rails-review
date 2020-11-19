1c71af570aad268b18803c2b647f7afdb64f2146
ashill99 <admshng@gmail.com>
2020-11-19 16:54:11 -0500

    nearly done

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # The Dunder Mifflin Pet Show
 
+Dogs -< Employee
 
 You're right on your way to help The Dunder Mifflin Paper Company with their brand new application. It is meant to 
 showcase each of the employees' pets. We’ve started some of the work for you, and we need your help to get our site fully working! Here are the deliverables:
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,21 @@
 class Employee < ApplicationRecord
-end
+    belongs_to :dog 
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true
+
+
+
+    def name
+        self.first_name + " " + self.last_name
+    end 
+
+    private 
+
+    # def is_unique
+    #     Employee.all.any? do  |employee| 
+    #         if employee.alias != self.alias || employee.title != self.title
+    #         end 
+    #     end 
+    # end 
+    
+end 
